69a89abcb150d7d31c8fc13135163ef6f8134552
LongNB8 <long17000547@gmail.com>
2020-01-13 10:47:22 +0700

    addbook

--- addbook.h
+++ addbook.h
@@ -14,9 +14,27 @@ class AddBook : public QWidget
 public:
     explicit AddBook(QWidget *parent = nullptr);
     ~AddBook();
+    void setIndexAuthor(int index);
+    int getIndexAuthor();
+
+    void setIndexGenre(int index);
+    int getIndexGenre();
+
+
+private slots:
+    void on_pushButtonAddBook_clicked();
+
+    void on_pushButtonLoad_clicked();
+
+    void on_comboBoxAuthor_currentIndexChanged(int index);
+
+    void on_comboBoxGenre_currentIndexChanged(int index);
 
 private:
     Ui::AddBook *ui;
+    int indexAuthor;
+    int indexGenre;
+
 };
 
 #endif // ADDBOOK_H
