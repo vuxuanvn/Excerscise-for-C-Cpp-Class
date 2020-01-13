915a910400dd705fb10b5d312668b5901d21fa6e
LongNB8 <long17000547@gmail.com>
2020-01-13 10:42:36 +0700

    searbook

--- searchbook.h
+++ searchbook.h
@@ -2,10 +2,11 @@
 #define SEARCHBOOK_H
 
 #include <QWidget>
-
+#include <QtSql>
 namespace Ui {
 class SearchBook;
 }
+class QSqlTableModel;
 
 class SearchBook : public QWidget
 {
@@ -18,8 +19,15 @@ public:
 private slots:
     void on_pushButtonSearch_clicked();
 
+    void on_pushButtonUpdate_clicked();
+
+    void on_pushButtonDelete_clicked();
+
 private:
     Ui::SearchBook *ui;
+    QSqlRelationalTableModel *model;
+    int authorIdx, genreIdx;
+
 };
 
 #endif // SEARCHBOOK_H
